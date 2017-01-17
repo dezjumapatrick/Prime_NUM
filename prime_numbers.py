5787e87070b9b8abd3858c0f6be0fa7cf92129f2
dezjumapatrick <dezjuma@gmail.com>
2017-01-17 15:30:47 +0300

    second commit

---
+++ prime_numbers.py
@@ -0,0 +1,12 @@
+n =int(input("Find Prime Numbers Upto What Number? : "))
+PrimeList =[]
+
+for x in range (0, n +1)
+	isPrime= True
+	for y in range(0, int(x ** 0.5)+1):
+		if x % y == 0:
+			isPrime= False
+			break
+			
+			if isPrime
+			PrimeList.append(x)
